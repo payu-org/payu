884036023c64723621300f36169f8c687940ea48
Marshall Ward <marshall.ward@gmail.com>
2012-05-29 17:56:29 +1000

    MITgcm restart support

--- mitgcm.py
+++ mitgcm.py
@@ -40,11 +40,66 @@ class mitgcm(Experiment):
     
     
     #---
-    def setup(self, use_symlinks=True, repeat_run=False):
+    def setup(self, days, dt, use_symlinks=True, repeat_run=False):
         
         # payu setup
         super(mitgcm, self).setup()
         
+        # Calculate time intervals
+        secs_per_day = 86400
+        n_timesteps = days * secs_per_day // dt
+        n_iter0 = (self.counter - 1) * n_timesteps
+        p_chkpt_freq = days * secs_per_day
+        
+        # Patch data timestep variables
+        data_path = os.path.join(self.work_path, 'data')
+        tmp_path = data_path + '~'
+        
+        tmp = open(tmp_path, 'w')
+        for line in open(data_path):
+            if line.lstrip().startswith('nIter0='):
+                tmp.write(' nIter0=%i,\n' % n_iter0)
+            elif line.lstrip().startswith('nTimeSteps='):
+                tmp.write(' nTimeSteps=%i,\n' % n_timesteps)
+            elif line.lstrip().startswith('deltaT='):
+                tmp.write(' deltaT=%i,\n' % dt)
+            elif line.lstrip().startswith('pChkptFreq='):
+                tmp.write(' pChkptFreq=%f,\n' % p_chkpt_freq)
+            else:
+                tmp.write(line)
+        tmp.close()
+        
+        sh.move(tmp_path, data_path)
+        
+        # Patch or create data.mnc
+        mnc_header = os.path.join(self.work_path, 'mnc_')
+        
+        data_mnc_path = os.path.join(self.work_path, 'data.mnc')
+        if os.path.exists(data_mnc_path):
+            tmp_path = data_mnc_path + '~'
+            tmp = open(tmp_path, 'w')
+            
+            for line in open(data_mnc_path):
+                if line.lstrip().startswith('mnc_outdir_str'):
+                    tmp.write(' mnc_outdir_str=\'%s\',\n' % mnc_header)
+                else:
+                    tmp.write(line)
+            tmp.close()
+            sh.move(tmp_path, data_mnc_path)
+        else:
+            data_mnc = open(data_mnc_path, 'w')
+            
+            data_mnc.write(' &MNC_01\n')
+            data_mnc.write(' mnc_use_outdir=.TRUE.,\n')
+            data_mnc.write(' mnc_use_name_ni0=.TRUE.,\n')
+            data_mnc.write(' mnc_outdir_str=\'%s\',\n' % mnc_header)
+            data_mnc.write(' mnc_outdir_date=.TRUE.,\n')
+            data_mnc.write(' monitor_mnc=.TRUE.,\n')
+            data_mnc.write(' &\n')
+            
+            data_mnc.close()
+        
+        # Link restart files to work directory
         if self.prior_run_path and not repeat_run:
             restart_files = [f for f in os.listdir(self.prior_run_path)
                              if f.startswith('pickup.')]
@@ -74,6 +129,19 @@ class mitgcm(Experiment):
         flags = flags + ('-mca mpi_affinity_alone 1',
                          '-wd %s' % self.work_path)
         super(mitgcm, self).run(*flags)
+
+        # Run cleanup
+        
+        # Move files outside of mnc_* directories
+        mnc_paths = [os.path.join(self.work_path, d)
+                     for d in os.listdir(self.work_path)
+                     if d.startswith('mnc_')]
+       
+        for path in mnc_paths:
+            for f in os.listdir(path):
+                f_path = os.path.join(path, f)
+                sh.move(f_path, self.work_path)
+            os.rmdir(path)
     
     #---
     def collate(self):
--- payu.py
+++ payu.py
@@ -232,8 +232,7 @@ class Experiment(object):
         cmd = ['qsub', self.model_script, '-v', '%s=%i,%s=%i'
                 % (counter_env, self.counter, max_counter_env,
                    self.max_counter) ]
-        # TODO: Remove .wait()?
-        sp.Popen(cmd).wait()
+        sp.Popen(cmd)
 
 
 #==============================================================================
