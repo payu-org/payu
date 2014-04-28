8588773a38829673f51e83903ea20de683a514e4
Nicholas Hannah <nchlshnnh@gmail.com>
2014-04-28 15:55:25 +1000

    Fix rebase mistakes.

--- payu/drivers/um.py
+++ payu/drivers/um.py
@@ -11,11 +11,8 @@ http://www.apache.org/licenses/LICENSE-2.0
 
 from payu.modeldriver import Model
 import os
-import sys
 import imp
 import fileinput
-import shlex
-import shutil
 
 class UnifiedModel(Model):
 
@@ -45,58 +42,6 @@ class UnifiedModel(Model):
     #---
     def archive(self):
         raise NotImplementedError
-
-    def set_model_pathnames(self):
-        super(UnifiedModel, self).set_model_pathnames()
-
-        ice_nml_path = os.path.join(self.control_path, self.ice_nml_fname)
-        self.ice_nmls = f90nml.read(ice_nml_path)
-
-        res_path = os.path.normpath(setup_nml['restart_dir'])
-        if not os.path.isabs(res_path):
-            res_path = os.path.join(self.work_path, res_path)
-
-        self.work_init_path = res_path
-        self.work_restart_path = res_path
-
-        work_out_path = os.path.normpath(setup_nml['history_dir'])
-        if not os.path.isabs(work_out_path):
-            work_out_path = os.path.join(self.work_path, work_out_path)
-
-        self.work_output_path = work_out_path
-
-        # Determine if there is a work input path
-        grid_nml = self.ice_nmls['grid_nml']
-        input_path, grid_fname = os.path.split(grid_nml['grid_file'])
-        if input_path and not input_path == '.':
-            assert not os.path.isabs(input_path)
-
-
-        self.work_input_path = os.path.join(self.work_path, input_path)
-
-
-    #---
-    def set_model_output_paths(self):
-        super(UnifiedModel, self).set_model_output_paths()
-
-        res_dir = self.ice_nmls['setup_nml']['restart_dir']
-
-        # Use the local initialization restarts if present
-        # TODO: Check for multiple res_paths across input paths?
-        if self.expt.counter == 0:
-            for input_path in self.input_paths:
-                if os.path.isabs(res_dir):
-                    init_res_path = res_dir
-                else:
-                    init_res_path = os.path.join(input_path, res_dir)
-                if os.path.isdir(init_res_path):
-                    self.prior_restart_path = init_res_path
-
-
-    #---
-    def get_prior_restart_files(self):
-        return [f for f in os.listdir(self.prior_restart_path)
-                if f.startswith('iced.')]
 
     #---
     def collate(self):
