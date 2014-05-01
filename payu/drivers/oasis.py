dd0242adcdc9cce5643162114eddbe4a6f3b89fd
Nicholas Hannah <nchlshnnh@gmail.com>
2014-05-01 16:29:57 +1000

    access-cm does not need have all the same restart files as access-om. For now only copy these over if they exist.

--- payu/drivers/oasis.py
+++ payu/drivers/oasis.py
@@ -13,6 +13,7 @@ http://www.apache.org/licenses/LICENSE-2.0
 import os
 import sys
 import shlex
+import shutil
 import subprocess
 
 # Local
@@ -75,10 +76,8 @@ class Oasis(Model):
             f_src = os.path.join(self.work_path, f)
             f_dst = os.path.join(self.restart_path, f)
 
-            cmd = 'mv {} {}'.format(f_src, f_dst)
-            rc = subprocess.call(shlex.split(cmd))
-            #assert rc == 0
-
+            if os.path.exists(f_src):
+                shutil.move(f_src, f_dst)
 
     #---
     def collate(self):
