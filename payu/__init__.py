65f93bcae4afc97bf6fc39f9084139a82b4e2d0e
Marshall Ward <marshall.ward@gmail.com>
2014-08-18 16:49:18 +1000

    version update

--- payu/__init__.py
+++ payu/__init__.py
@@ -4,8 +4,8 @@
 
    A climate model workflow managament tool
 
-   :copyright: Copyright 2011-2014 Marshall Ward
+   :copyright: Copyright 2011-2014 Marshall Ward, see AUTHORS for details
    :license: Apache License, Version 2.0, see LICENSE for details
 """
 
-__version__ = '0.2'
+__version__ = '0.3-dev'
