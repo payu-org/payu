e69de6e2bc3f440126332aa5b2aa2a95e505e8d1
Marshall Ward <marshall.ward@gmail.com>
2015-08-30 11:00:48 +1000

    Added the darshan profiler support class

---
+++ payu/profilers/darshan.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+from payu.profilers.profiler import Profiler
+from payu.fsops import mkdir_p
+
+
+class Darshan(Profiler):
+
+    def __init__(self, expt):
+        super(Darshan, self).__init__(expt)
+        # TODO: Generalise this
+        self.lib_path = '/short/fp0/mxw900/libs/libdarshan.so'
+
+    def setup(self):
+        ld_preload = os.environ.get('LD_PRELOAD', '')
+        os.environ['LD_PRELOAD'] = ':'.join([self.lib_path, ld_preload])
+
+    def postprocess(self):
+        pass
