5619d2588c58edea1a27c470d674dd739eda755e
Marshall Ward <marshall.ward@gmail.com>
2015-11-24 12:13:33 +1100

    Dummy test script added

---
+++ test/test_payu.py
@@ -0,0 +1,18 @@
+import sys
+import unittest
+
+sys.path.insert(1, '../')
+import payu
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        # Data here
+        pass
+
+    def test_dummy(self):
+        # Start tests with `test_`
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
