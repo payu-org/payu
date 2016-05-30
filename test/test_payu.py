b372aae77c4b421cb8466970520872d9d9a347d2
Marshall Ward <marshall.ward@gmail.com>
2016-05-30 11:46:04 +1000

    mkdir_p exception testing

--- test/test_payu.py
+++ test/test_payu.py
@@ -1,4 +1,6 @@
+import errno
 import os
+import stat
 import sys
 import unittest
 
@@ -23,11 +25,18 @@ class Test(unittest.TestCase):
         # Re-create existing directory
         payu.fsops.mkdir_p(tmp_dir)
 
+        # Raise a non-EEXIST error (EACCES)
+        tmp_tmp_dir = os.path.join(tmp_dir, 'more_tmp')
+        os.chmod(tmp_dir, stat.S_IRUSR)
+        self.assertRaises(OSError, payu.fsops.mkdir_p, tmp_tmp_dir)
+
+        # Cleanup
+        os.chmod(tmp_dir, stat.S_IWUSR)
         os.rmdir(tmp_dir)
 
     def test_read_config(self):
-        # TODO
-        pass
+        config_path = 'config_mom5.yaml'
+        config = payu.fsops.read_config(config_path)
 
     def test_default_lab_path(self):
         # TODO
