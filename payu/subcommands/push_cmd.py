ba79d97369e358e2fd84abf702fc1bf705a4bc9a
Marshall Ward <marshall.ward@gmail.com>
2017-06-23 11:12:38 +1000

    Push subcommand added

---
+++ payu/subcommands/push_cmd.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from payu.experiment import Experiment
+from payu.laboratory import Laboratory
+import payu.subcommands.args as args
+
+title = 'push'
+parameters = {'description': 'Push configuration to github'}
+
+arguments = [args.model, args.config, args.laboratory]
+
+
+def runcmd(model_type, config_path, lab_path):
+
+    lab = Laboratory(model_type, config_path, lab_path)
+    expt = Experiment(lab)
+    runlog = Runlog(expt)
+
+    runlog.push()
+
+
+runscript = runcmd
