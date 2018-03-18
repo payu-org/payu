23af7defd481efb6418462ee04ef2091d5aa282b
Marshall Ward <marshall.ward@gmail.com>
2018-03-19 10:46:41 +1100

    Version update, release notes

--- payu/__init__.py
+++ payu/__init__.py
@@ -7,4 +7,4 @@
    :copyright: Copyright 2011 Marshall Ward, see AUTHORS for details.
    :license: Apache License, Version 2.0, see LICENSE for details.
 """
-__version__ = '0.8'
+__version__ = '0.8.1'
