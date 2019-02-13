a97096811ca18c80951384ee6f684bc38f52df1b
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2019-02-13 14:50:50 +1100

    cice: Added optional input_ice.nml for ACCESS-ESM
    cice: Made copy_inputs default False
    
    cice: Added logic for init_path (where restarts are read from)
    
    cice: Added checks for consistent paths.
    
    cice: commented out get_prior_restart_files as it was returning only one
    iced file, ignoring other restarts. Added in logic later to only pick up
    latest restart when it is required.
    
    cice: Moved some of the restart cleaning logic from cice5 to cice so
    iced restarts don't accumulate.
    
    cice: Cleaned more paths of symlinks in archive

--- payu/models/cice.py
+++ payu/models/cice.py
@@ -43,11 +43,14 @@ class Cice(Model):
         self.repo_url = 'https://github.com/CWSL/cice4.git'
         self.repo_tag = 'access'
 
-        self.config_files = ['ice_in']
+        self.config_files = ['cice_in.nml']
+        self.optional_config_files = ['input_ice.nml']
 
-        self.ice_nml_fname = 'ice_in'
+        self.ice_nml_fname = 'cice_in.nml'
 
         self.set_timestep = self.set_local_timestep
+
+        self.copy_inputs = False
 
     def set_model_pathnames(self):
         super(Cice, self).set_model_pathnames()
@@ -67,10 +70,14 @@ class Cice(Model):
         if input_dir is None:
             # Default to reading and writing inputs/restarts in-place
             input_path = res_path
+            init_path = res_path
         else:
             input_path = os.path.normpath(input_dir)
+            init_path = input_path
 
-        # Determine if there is a work input path from grid path
+        # Determine if there is a work input path from the path to the
+        # grid.nc file. Older cice versions don't have a defined INPUT
+        # directory, but it is implied by this path
         grid_nml = self.ice_in['grid_nml']
         path, _ = os.path.split(grid_nml['grid_file'])
         if path and not path == os.path.curdir:
@@ -79,47 +86,35 @@ class Cice(Model):
             # Get input_dir from grid_file path unless otherwise specified
             if input_dir is None:
                 input_path = path
+            else:
+                if path != input_path:
+                    print('payu: error: Grid file path in {nmlfile} '
+                          '({path}) does not match input path '
+                          '({inputpath})'.format(
+                            nmlfile=self.ice_nml_fname,
+                            path=path,
+                            inputpath=input_path))
+                    sys.exit(1)
 
         # Check for consistency in input paths due to cice having the same
         # information in multiple locations
+        path, _ = os.path.split(self.ice_in['grid_nml'].get('kmt_file'))
+        path = os.path.normpath(path)
         if path != input_path:
             print('payu: error: '
-                  'Grid file path in {nmlfile} ({gridpath}) does not match '
+                  'kmt file path in {nmlfile} ({path}) does not match '
                   'input path ({inputpath})'.format(
                     nmlfile=self.ice_nml_fname,
-                    gridpath=path,
+                    path=path,
                     inputpath=input_path))
             sys.exit(1)
 
-        # Check paths are consistent in namelist
-        tmp_path, _ = os.path.split(self.ice_in['grid_nml'].get('kmt_file'))
-        tmp_path = os.path.normpath(tmp_path)
-        if not path == tmp_path:
-            print('payu: error: '
-                  'Paths for kmt_file ({varpath}) and grid_file '
-                  '({gridpath}) in {nmlfile} do not '
-                  'match'.format(varpath=tmp_path,
-                                 gridpath=path,
-                                 nmlfile=self.ice_nml_fname))
-            sys.exit(1)
-
-        # Check paths are consistent in namelist
-        tmp_path, _ = os.path.split(
-            self.ice_in['setup_nml'].get('pointer_file')
-        )
-        tmp_path = os.path.normpath(tmp_path)
-        if not path == tmp_path:
-            print('payu: warning: '
-                  'Paths for pointer_file ({varpath}) and grid_file '
-                  '({gridpath}) in {nmlfile} do not '
-                  'match'.format(varpath=tmp_path,
-                                 gridpath=path,
-                                 nmlfile=self.ice_nml_fname))
-
         if not os.path.isabs(input_path):
             input_path = os.path.join(self.work_path, input_path)
+        if not os.path.isabs(init_path):
+            init_path = os.path.join(self.work_path, init_path)
         self.work_input_path = input_path
-        self.work_init_path = self.work_input_path
+        self.work_init_path = init_path
 
         if not os.path.isabs(res_path):
             res_path = os.path.join(self.work_path, res_path)
@@ -130,6 +125,12 @@ class Cice(Model):
         if not os.path.isabs(work_out_path):
             work_out_path = os.path.join(self.work_path, work_out_path)
         self.work_output_path = work_out_path
+
+        self.split_paths = (self.work_input_path != self.work_restart_path)
+
+        if self.split_paths:
+            self.copy_inputs = False
+            self.copy_restarts = False
 
     def set_model_output_paths(self):
         super(Cice, self).set_model_output_paths()
@@ -147,13 +148,13 @@ class Cice(Model):
                 if os.path.isdir(init_res_path):
                     self.prior_restart_path = init_res_path
 
-    def get_prior_restart_files(self):
-        restart_files = [f for f in os.listdir(self.prior_restart_path)
-                         if f.startswith('iced.')]
-        return [sorted(restart_files)[-1]]
+    # def get_prior_restart_files(self):
+    #     restart_files = [f for f in os.listdir(self.prior_restart_path)
+    #                      if f.startswith('iced.')]
+    #     return [sorted(restart_files)[-1]]
 
     def get_ptr_restart_dir(self):
-        return os.path.relpath(self.work_input_path, self.work_path)
+        return os.path.relpath(self.work_init_path, self.work_path)
 
     def get_access_ptr_restart_dir(self):
         # The ACCESS build of CICE assumes that restart_dir is 'RESTART'
@@ -165,13 +166,19 @@ class Cice(Model):
 
         # Change perms of restart files, these get overwritten so much be
         # writable.
-        for fname in ['o2i.nc', 'i2o.nc', 'u_star.nc', 'grid.nc', 'kmt.nc',
-                      'monthly_sstsss.nc', 'mice.nc']:
-            path = os.path.join(self.work_input_path, fname)
-            if os.path.exists(path):
-                perm = (stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
-                        | stat.S_IWUSR)
-                os.chmod(path, perm)
+
+        if self.copy_restarts:
+            for fname in ['o2i.nc', 'i2o.nc', 'u_star.nc', 'grid.nc', 'kmt.nc',
+                        'monthly_sstsss.nc', 'mice.nc']:
+                path = os.path.join(self.work_input_path, fname)
+                if os.path.exists(path):
+                    try:
+                        perm = (stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
+                                | stat.S_IWUSR)
+                        os.chmod(path, perm)
+                    except:
+                        print("Couldn't change permissions of {}".format(path))
+                        pass
 
         setup_nml = self.ice_in['setup_nml']
         init_date = datetime.date(year=setup_nml['year_init'], month=1, day=1)
@@ -185,15 +192,17 @@ class Cice(Model):
             # Generate ice.restart_file
             # TODO: better check of restart filename
             iced_restart_file = None
-            for f in self.get_prior_restart_files():
-                if 'iced.' in f:
-                    iced_restart_file = f
+            iced_restart_files = [ f for f in self.get_prior_restart_files() 
+                                   if f.startswith('iced.') ]
+
+            if len(iced_restart_files) > 0:
+                iced_restart_file = sorted(iced_restart_files)[-1]
 
             if iced_restart_file is None:
                 print('payu: error: No restart file available.')
                 sys.exit(errno.ENOENT)
 
-            res_ptr_path = os.path.join(self.work_input_path,
+            res_ptr_path = os.path.join(self.work_init_path,
                                         'ice.restart_file')
             with open(res_ptr_path, 'w') as res_ptr:
                 res_dir = self.get_ptr_restart_dir()
@@ -296,12 +305,31 @@ class Cice(Model):
 
     def archive(self, **kwargs):
 
-        for f in os.listdir(self.work_input_path):
-            f_path = os.path.join(self.work_input_path, f)
-            if os.path.islink(f_path):
-                os.remove(f_path)
+        # Find and delete all the (MANY!) symlinks
+        for d in [self.work_path, self.work_input_path, 
+                  self.work_init_path, self.work_restart_path]:
+            for f in os.listdir(d):
+                f_path = os.path.join(d, f)
+                if os.path.islink(f_path):
+                    os.remove(f_path)
 
         os.rename(self.work_restart_path, self.restart_path)
+
+        if not self.split_paths:
+            res_ptr_path = os.path.join(self.restart_path, 'ice.restart_file')
+            with open(res_ptr_path) as f:
+                res_name = os.path.basename(f.read()).strip()
+
+            assert os.path.exists(os.path.join(self.restart_path, res_name))
+
+            # Delete the old restart file (keep the one in ice.restart_file)
+            for f in self.get_prior_restart_files():
+                if f.startswith('iced.'):
+                    if f == res_name:
+                        continue
+                    os.remove(os.path.join(self.restart_path, f))
+        else:
+            shutil.rmtree(self.work_input_path)
 
     def collate(self):
         pass
