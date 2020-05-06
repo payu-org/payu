ace9c88ddafecf1470f042b2fd8c0e5cccf96a38
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2020-05-06 12:00:10 +1000

    Another PEP8 fix

--- payu/subcommands/run_cmd.py
+++ payu/subcommands/run_cmd.py
@@ -15,7 +15,7 @@ arguments = [args.model, args.config, args.initial, args.nruns,
              args.laboratory, args.reproduce, args.force]
 
 
-def runcmd(model_type, config_path, init_run, n_runs, lab_path, 
+def runcmd(model_type, config_path, init_run, n_runs, lab_path,
            reproduce, force):
 
     # Get job submission configuration
