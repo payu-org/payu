2f6b570d1f7b0142a1b5a90446325bd0f0ddd547
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2020-05-13 10:39:04 +1000

    Fixed line length of conf file that PEP8 decided was too long

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,10 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(os.pardir)))
 
 # Sphinx setup
 #needs_sphinx = '1.0'
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel']
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.ifconfig', 
+              'sphinx.ext.viewcode', 
+              'sphinx.ext.autosectionlabel']
 autosectionlabel_maxdepth = 2
 templates_path = ['_templates']
 source_suffix = '.rst'
