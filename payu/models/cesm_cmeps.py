b2e7a16e8155cf57a2ea7ab556ac73d924994fb6
dougiesquire <dougiesquire@gmail.com>
2023-05-25 09:16:05 +1000

    get components from nuopc.runconfig

--- payu/models/cesm_cmeps.py
+++ payu/models/cesm_cmeps.py
@@ -10,22 +10,20 @@ http://www.apache.org/licenses/LICENSE-2.0
 """
 from __future__ import print_function
 
-# Standard Library
 import os
 import re
+import errno
 import glob
 import shutil
 import multiprocessing
 
-# Local
 from payu.fsops import mkdir_p
 from payu.models.model import Model
 from payu.models.fms import fms_collate
 
 # Add as needed
 component_info = {
-    "mom6": {
-        "realm": "ocn",
+    "mom": {
         "config_files": [
             "input.nml",
             "MOM_input",
@@ -33,30 +31,25 @@ component_info = {
         ],
         "optional_config_files" : ["MOM_override"]
     },
-    "cice6": {
-        "realm": "ice",
+    "cice": {
         "config_files": ["ice_in"],
     },
-    "ww3": {
-        "realm": "wav",
+    "ww3dev": {
         "config_files": ["wav_in"],
     },
     "datm": {
-        "realm": "atm",
         "config_files": [
             "datm_in",
             "datm.streams.xml",
         ],
     },
     "docn": {
-        "realm": "ocn",
         "config_files": [
             "docn_in",
             "docn.streams.xml",
         ],
     },
     "drof": {
-        "realm": "rof",
         "config_files": [
             "drof_in",
             "drof.streams.xml"
@@ -64,41 +57,51 @@ component_info = {
     },
 }
 
-class CesmCmepsBase(Model):
+class CesmCmeps(Model):
 
-    def __init__(self, components, expt, name, config):
+    def __init__(self, expt, name, config):
         super().__init__(expt, name, config)
 
         self.model_type = 'cesm-cmeps'
 
-        self.components = components
-        self.realms = ["cpl"] + [
-            component_info[component]["realm"] for component in self.components
-            ]
-        self.config_files, self.optional_config_files = self.get_component_config_files()
-
-    def get_component_config_files(self):
-
-        assert self.components is not None, "Model components must be specified for CesmCmepsBase"
-
-        # Driver config files always present
-        config_files = [
+        # Driver config files should always be present
+        self.config_files = [
             "drv_in",
             "fd.yaml",
             "nuopc.runconfig",
             "nuopc.runseq"
         ]
-        optional_config_files = []
-        for component in self.components:
-            config_files.extend(component_info[component]["config_files"])
-            try:
-                optional_config_files.extend(
-                    component_info[component]["optional_config_files"]
-                    )
-            except KeyError:
-                pass
 
-        return config_files, optional_config_files
+        self.realms = ["ocn", "ice", "wav", "atm", "rof", "cpl"]
+        self.runconfig = None # nuopc.runconfig. Cna't read this yet as work_path hasn't been set
+        self.components = {} # To be read from nuopc.runconfig
+        self.rpointers = [] # To be inferred from nuopc.runconfig
+
+    def get_components(self):
+        """Get components from nuopc.runconfig"""
+
+        self.runconfig = Runconfig(os.path.join(self.work_path, 'nuopc.runconfig'))
+
+        self.components = {}
+        self.rpointers = ["rpointer.cpl"]
+        for realm in self.realms:
+            if realm != "cpl":
+                component = self.runconfig.get("ALLCOMP_attributes", f"{realm.upper()}_model")
+                if not component.startswith("s"): # TODO: better check for stub component?
+                    self.components[realm] = component
+                    self.rpointers.append(f"rpointer.{realm}")
+
+        for component in self.components.values():
+            if component in component_info:
+                self.config_files.extend(component_info[component]["config_files"])
+                try:
+                    self.optional_config_files.extend(
+                        component_info[component]["optional_config_files"]
+                        )
+                except KeyError:
+                    pass
+            else:
+                raise ValueError(f"There is no component info entry for {component}")
 
     def set_model_pathnames(self):
 
@@ -109,18 +112,30 @@ class CesmCmepsBase(Model):
     def setup(self):
         super().setup()
 
-        runconfig = Runconfig(os.path.join(self.work_path, 'nuopc.runconfig'))
+        # Read components from nuopc.runconfig and copy component configuration files
+        self.get_components()
+        for f_name in self.config_files:
+            f_path = os.path.join(self.control_path, f_name)
+            shutil.copy(f_path, self.work_path)
 
-        self.case_name = runconfig.get("ALLCOMP_attributes", "case_name")
-
+        for f_name in self.optional_config_files:
+            f_path = os.path.join(self.control_path, f_name)
+            try:
+                shutil.copy(f_path, self.work_path)
+            except IOError as exc:
+                if exc.errno == errno.ENOENT:
+                    pass
+                else:
+                    raise
+        
         if self.prior_restart_path and not self.expt.repeat_run:
             start_type = 'continue'
 
             # Overwrite restart pointer symlinks with copies
             pointer_files = [
                 os.path.join(
-                    self.work_path, f"rpointer.{realm}",
-                    ) for realm in self.realms
+                    self.work_path, pointer,
+                    ) for pointer in self.rpointers
             ]
             for f_dst in pointer_files:
                 f_src = os.readlink(f_dst)
@@ -129,31 +144,31 @@ class CesmCmepsBase(Model):
         else:
             start_type = 'startup'
 
-        runconfig.set("ALLCOMP_attributes", "start_type", start_type)
+        self.runconfig.set("ALLCOMP_attributes", "start_type", start_type)
 
         # Check pelayout makes sense
-        all_realms = ["atm", "cpl", "glc", "ice", "lnd", "ocn", "rof", "wav"]
+        all_realms = self.realms + ["glc", "lnd"]
         cpucount = int(
             self.expt.config.get('ncpus', multiprocessing.cpu_count())
             )
         for realm in all_realms:
-            ntasks = int(runconfig.get("PELAYOUT_attributes", f"{realm}_ntasks"))
+            ntasks = int(self.runconfig.get("PELAYOUT_attributes", f"{realm}_ntasks"))
             assert cpucount >= ntasks, "Insufficient cpus for the pelayout in nuopc.runconfig"
         
         # Ensure that restarts will be written at the end of each run
-        stop_n = runconfig.get("CLOCK_attributes", "stop_n")
-        stop_option = runconfig.get("CLOCK_attributes", "stop_option")
-        runconfig.set("CLOCK_attributes", "restart_n", stop_n)
-        runconfig.set("CLOCK_attributes", "restart_option", stop_option)
+        stop_n = self.runconfig.get("CLOCK_attributes", "stop_n")
+        stop_option = self.runconfig.get("CLOCK_attributes", "stop_option")
+        self.runconfig.set("CLOCK_attributes", "restart_n", stop_n)
+        self.runconfig.set("CLOCK_attributes", "restart_option", stop_option)
         
         mkdir_p(os.path.join(self.work_path, 'log'))
         mkdir_p(os.path.join(self.work_path, 'timing'))
 
-        runconfig.write()
+        self.runconfig.write()
 
-        # TODO: Need a better way to do this
+        # TODO: Should have a better way to do this
         # The ww3 mod_def input needs to be in work_path and called mod_def.ww3
-        if "ww3" in self.components:
+        if "ww3dev" in self.components.values():
             files =  glob.glob(
                 os.path.join(self.work_input_path, "*mod_def.ww3*"),
                 recursive=False
@@ -175,17 +190,17 @@ class CesmCmepsBase(Model):
         # handled in the same way.
         # Note, I don't actually know how WW3 knows what restart to use. The normal approach to
         # providing restarts to WW3 is to add a restart.ww3 file to the run directory. However, here
-        # WW3 looks for a <case_name>.ww3.r.<date> file despite this not  being specified in wav_in.
+        # WW3 looks for a <case_name>.ww3.r.<date> file despite this not being specified in wav_in.
         # I guess the driver specifies this somewhere based on the cpl restart file name?
-        if "ww3" in self.components:
+        if "ww3dev" in self.components.values():
             cpl_pointer = os.path.join(
-                self.work_path, f"rpointer.cpl",
+                self.work_path, "rpointer.cpl",
                 )
             with open(cpl_pointer, "r") as f:
                 cpl_restart = f.readline().rstrip()
-                wav_restart = cpl_restart.replace("cpl", "ww3").removesuffix(".nc")
+            wav_restart = cpl_restart.replace("cpl", "ww3").removesuffix(".nc")
             wav_pointer = os.path.join(
-                self.work_path, f"rpointer.wav",
+                self.work_path, "rpointer.wav",
                 )
             with open(wav_pointer, 'w') as f:
                 f.write(wav_restart)
@@ -193,8 +208,8 @@ class CesmCmepsBase(Model):
         # Get names of restarts from restart pointer files
         pointer_files = [
                 os.path.join(
-                    self.work_path, f"rpointer.{realm}",
-                    ) for realm in self.realms
+                    self.work_path, pointer,
+                    ) for pointer in self.rpointers
             ]
         restart_files = []
         for pointer in pointer_files:
@@ -215,17 +230,30 @@ class CesmCmepsBase(Model):
             f_dst = os.path.join(self.restart_path, name)
             shutil.move(f_src, f_dst)
 
-
-class AccessOm3(CesmCmepsBase):
-
-    def __init__(self, expt, name, config):
-
-        components = ["mom6", "cice6", "ww3", "datm", "drof"]
-        
-        super().__init__(components, expt, name, config)
-
     def collate(self):
-        fms_collate(self)
+        
+        # .setup is not run when collate is called so need to get components
+        self.get_components()
+        
+        if "mom" in self.components.values():
+            fms_collate(self)
+        else:
+            super().collate()
+
+
+class AccessOm3(CesmCmeps):
+
+    def get_components(self):
+        super().get_components()
+
+        assert self.components["atm"] == "datm", (
+            "Access-OM3 comprises a data atmosphere model, but the atmospheric model in nuopc.runconfig is set "
+            f"to {self.components['atm']}."
+        )
+        assert self.components["rof"] == "drof", (
+            "Access-OM3 comprises a data runoff model, but the runoff model in nuopc.runconfig is set "
+            f"to {self.components['rof']}."
+        )
 
 
 class Runconfig:
