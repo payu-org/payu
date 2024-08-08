bec380594f3cb1da5dabedd6eb00b173953f2135
Whyborn <lachlan.s.whyborn@gmail.com>
2024-08-08 15:39:23 +1000

    Apply some suggestions from Aidan and some corrections caught by testing

--- payu/models/staged_cable.py
+++ payu/models/staged_cable.py
@@ -10,6 +10,7 @@
 # Standard Library
 import os
 import shutil
+import itertools
 
 # Extensions
 import f90nml
@@ -60,12 +61,6 @@ class StagedCable(Model):
             # Read the current configuration log
             self._read_configuration_log()
 
-        # Add the restart directories to inputs
-        num_completed_stages = len(self.configuration_log['completed_stages'])
-        for restart_dir in reversed(range(num_completed_stages)):
-            config['input'].append(os.path.realpath(
-                f'archive/output{restart_dir:03d}/restart'))
-
         # Now set the number of runs using the configuration_log
         remaining_stages = len(self.configuration_log['queued_stages'])
         print("Overriding the remaining number of runs according to the " +
@@ -109,26 +104,27 @@ class StagedCable(Model):
                 # looks like [S1, S2, S1, S2, S1, S2, S1, S1].
                 # So what we need to do is first record the number
                 # of times each step is run
-                step_names = []
-                step_counts = []
-                for step_name, step_opts in stage_opts.items():
-                    step_names.append(step_name)
-                    step_counts.append(step_opts['count'])
 
-                # Now iterate to the maximum number of steps
-                for step in range(max(step_counts)):
-                    for step_id, _ in enumerate(step_names):
-                        if step_counts[step_id] > step:
-                            cable_stages.append(step_names[step_id])
-            # Finish handling of multistep stage
+                # Use a recipe from https://stackoverflow.com/questions/48199961
+                # Turn the stages into lists of "count" length
+                steps = [[step_name] * stage_opts[step_name]['count']
+                        for step_name in stage_opts.keys()]
+
+                cable_stages.extend([stage for stage in 
+                        itertools.chain.from_iterable(
+                            itertools.zip_longest(
+                                *steps
+                            )
+                        )
+                        if stage is not None])
+                # Finish handling of multistep stage
 
             else:
                 # A single step stage, in general we only want to run this
                 # once, but check for the count anyway
-                for _ in range(stage_opts['count']):
-                    cable_stages.append(stage_name)
+                cable_stages.extend([stage_name] * stage_opts['count'])
 
-        # Finish handling of single step stage
+            # Finish handling of single step stage
         return cable_stages
 
     def setup(self):
@@ -146,17 +142,30 @@ class StagedCable(Model):
         # Go to the archives of the previous completed stages and retrieve
         # the files from them, with the most recent taking precedent.
 
-        restart_files = []
+        # Unfortunately, we can't simply use the 
+        # "if {filename} not in {restart_files}, because files from different
+        # stages will have different paths, even if the local file name is the
+        # same. To avoid having to call os.path.basepath on the list of restart
+        # files for every addition, we'll store the list of local file names and
+        # paths separately, and pull them together at the end.
+
+        file_names = []
+        path_names = []
+
         num_completed_stages = len(self.configuration_log['completed_stages'])
 
         for stage_number in reversed(range(num_completed_stages)):
-            respath = f'archive/output{stage_number:03d}/restart'
-            # We can't simply extend restart_files with os.listdir(respath)
-            # because we don't want multiple copies of the same file name, which
-            # will often occur. We only want the most recent version of a given
-            # file.
-            [restart_files.append(file) for file in os.listdir(respath)
-                    if file not in restart_files]
+            respath = os.path.join(
+                    self.control_path,
+                    f'archive/output{stage_number:03d}/restart'
+            )
+
+            [(file_names.append(file), path_names.append(respath))
+                    for file in os.listdir(respath) if file not in file_names]
+
+        # Zip up the files
+        restart_files = [os.path.join(path, file)
+                for path, file in zip(path_names, file_names)]
 
         return restart_files
 
@@ -176,7 +185,7 @@ class StagedCable(Model):
             yaml.dump(self.configuration_log, conf_log_f)
 
         # Ensure the directory exists
-        if not os.path.isdir(stage_name):
+        if not os.path.isdir(os.path.join(self.control_path, stage_name)):
             errmsg = f"""Directory containing namelists for stage {stage_name}
              does not exist."""
             raise FileNotFoundError(errmsg)
@@ -189,14 +198,16 @@ class StagedCable(Model):
 
     def _apply_stage_namelists(self, stage_name):
         """Apply the stage namelists to the master namelists."""
-        namelists = os.listdir(stage_name)
+        namelists = os.listdir(os.path.join(self.control_path, stage_name))
 
         for namelist in namelists:
             write_target = os.path.join(self.work_input_path, namelist)
-            with open(os.path.join(stage_name, namelist)) as stage_nml_f:
+            stage_nml = os.path.join(self.control_path, stage_name, namelist)
+            with open(stage_nml) as stage_nml_f:
                 stage_namelist = f90nml.read(stage_nml_f)
 
-            f90nml.patch(namelist, stage_namelist, write_target)
+            master_nml = os.path.join(self.control_path, namelist)
+            f90nml.patch(master_nml, stage_namelist, write_target)
 
     def archive(self):
         """Store model output to laboratory archive and update the
@@ -207,9 +218,9 @@ configuration log."""
         self.configuration_log['current_stage'] = ''
         self.configuration_log['completed_stages'].append(completed_stage)
 
-        self.save_configuration_log()
+        self._save_configuration_log()
 
-        if int(os.environ['PAYU_N_RUNS']) == 1:
+        if int(os.environ['PAYU_N_RUNS']) == 0:
             os.remove('configuration_log.yaml')
 
         super(StagedCable, self).archive()
