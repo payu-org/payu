ae869fe3819a9acb12e6255e28e2e7cffe608348
anton-seaice <anton-seaice@noreply.github.com>
2024-09-09 10:34:19 +1000

    draft of payu clone tests for cice driver

--- test/models/test_cice.py
+++ test/models/test_cice.py
@@ -5,6 +5,9 @@ import pytest
 import f90nml
 
 import payu
+from payu.branch import clone
+import git
+
 
 from test.common import cd
 from test.common import tmpdir, ctrldir, labdir, expt_workdir, ctrldir_basename
@@ -31,6 +34,7 @@ DEFAULT_CICE_NML = {
 }
 CICE_NML_NAME = "cice_in.nml"
 HIST_NML_NAME = "ice_history.nml"
+RESTART_NAME = "./RESTART/iced.r"
 
 
 def setup_module(module):
@@ -81,36 +85,61 @@ def teardown_module(module):
     except Exception as e:
         print(e)
 
+@pytest.fixture
+def cice_config_files(request):
+    cice_nml = DEFAULT_CICE_NML
+    if hasattr(request,'param'): 
+        ice_history = request.param
+    else:
+        ice_history = None
+
+    # create the files parsed by setup:
+    # 1. a restart pointer file
+    with cd(expt_workdir):
+        os.mkdir(cice_nml["setup_nml"]["restart_dir"])
+
+        with open(RESTART_NAME, "w") as f:
+            f.write("blank")
+            f.close()
+
+        with open(cice_nml["setup_nml"]["pointer_file"], "w") as f:
+            f.write(RESTART_NAME)
+            f.close()
+
+    with cd(ctrldir):
+        # 2. Create config.nml
+        f90nml.write(cice_nml, CICE_NML_NAME)
+
+        if ice_history:
+            f90nml.write(ice_history, HIST_NML_NAME)
+
+    yield {'ice_history': ice_history}
+
+    # cleanup
+    with cd(expt_workdir):
+        shutil.rmtree(cice_nml["setup_nml"]["restart_dir"])
+    with cd(ctrldir):
+        os.remove(CICE_NML_NAME)
+        if ice_history:
+            os.remove(HIST_NML_NAME)
+
+        
 
 # Confirm that 1: payu overwrites cice_in with ice_history
 # 2: payu works without ice_history.nml
 # 3: payu overwrites cice_in and allows additional fields
 # In all cases confirm dump_last is not added to model_type='cice'
 @pytest.mark.parametrize(
-    "ice_history",
+    "cice_config_files",
     [
         {"icefields_nml": { "f_icy": "m" }},
         False,
         {"icefields_nml": {"f_icy": "m", "f_new": "y"}},
-    ],
+    ], indirect=True
 )
-def test_setup(ice_history):
-    cice_nml = DEFAULT_CICE_NML
-
-    # create the files parsed by setup:
-    # 1. a restart pointer file
-    with cd(expt_workdir):
-        os.mkdir(cice_nml["setup_nml"]["restart_dir"])
-        with open(cice_nml["setup_nml"]["pointer_file"], "w") as f:
-            f.write("./RESTART/ice.r")
-            f.close()
+def test_setup(cice_config_files):
 
     with cd(ctrldir):
-        # 2. Create config.nml
-        f90nml.write(cice_nml, CICE_NML_NAME)
-        if ice_history:
-            f90nml.write(ice_history, HIST_NML_NAME)
-
         lab = payu.laboratory.Laboratory(lab_path=str(labdir))
         expt = payu.experiment.Experiment(lab, reproduce=False)
         model = expt.models[0]
@@ -121,12 +150,13 @@ def test_setup(ice_history):
     # Check config files are moved to model's work path
     work_path_files = os.listdir(model.work_path)
     assert CICE_NML_NAME in work_path_files
+    assert HIST_NML_NAME not in work_path_files
 
     # Check cice_in was patched with ice_history
     work_input_fpath = os.path.join(model.work_path, CICE_NML_NAME)
     input_nml = f90nml.read(work_input_fpath)
-    if ice_history:
-        assert input_nml["icefields_nml"] == ice_history["icefields_nml"]
+    if cice_config_files['ice_history']:
+        assert input_nml["icefields_nml"] == cice_config_files["ice_history"]["icefields_nml"]
     else:
         assert input_nml["icefields_nml"] == DEFAULT_CICE_NML["icefields_nml"]
 
@@ -134,11 +164,128 @@ def test_setup(ice_history):
     with pytest.raises(KeyError, match="dump_last"):
         input_nml["setup_nml"]["dump_last"]
 
-    # cleanup
+
+#test that pytest raises an error if it can't find the restart pointer
+def test_no_restart_ptr(cice_config_files):
+
     with cd(expt_workdir):
-        os.remove(cice_nml["setup_nml"]["pointer_file"])
-        os.rmdir(cice_nml["setup_nml"]["restart_dir"])
+        os.remove(DEFAULT_CICE_NML["setup_nml"]["pointer_file"])
+
+    assert 'RESTART' in os.listdir(expt_workdir)
+
     with cd(ctrldir):
-        os.remove(CICE_NML_NAME)
-        if ice_history:
-            os.remove(HIST_NML_NAME)
+            
+        lab = payu.laboratory.Laboratory(lab_path=str(labdir))
+        expt = payu.experiment.Experiment(lab, reproduce=False)
+        model = expt.models[0]
+
+        # Function to test
+        with pytest.raises(RuntimeError):
+            model.setup()
+
+
+@pytest.mark.parametrize("cice_config_files", 
+    [False, {"icefields_nml": { "f_icy": "m" }}],
+    indirect=True)
+def test_clone(cice_config_files):
+    with cd(ctrldir):
+            
+        lab = payu.laboratory.Laboratory(lab_path=str(labdir))
+        expt = payu.experiment.Experiment(lab, reproduce=False)
+        model = expt.models[0]
+
+        # Function to test
+        model.setup()
+
+    # Initialise a control repo
+    repo = git.Repo.init(ctrldir)
+    repo.index.add("*")
+    # Commit the changes
+    repo.index.commit("First commit - initialising repository")
+    source_main = str(repo.active_branch)
+
+    # Clone
+    cloned_repo_path = tmpdir / "clonedRepo"
+    clone(str(ctrldir), cloned_repo_path, lab_path=labdir)
+
+    cloned_repo = git.Repo(cloned_repo_path)
+    cloned_repo.git.checkout(source_main)
+
+    ctrl_path_files = os.listdir(cloned_repo_path)
+    assert CICE_NML_NAME in ctrl_path_files
+
+    if cice_config_files['ice_history']:
+        assert HIST_NML_NAME in ctrl_path_files
+
+    # Setup
+
+    with cd(cloned_repo_path):
+
+        lab = payu.laboratory.Laboratory(lab_path=str(labdir))
+        expt = payu.experiment.Experiment(lab, reproduce=False)
+        model = expt.models[0]
+
+        # Function to test
+        model.setup()
+
+    work_path_files = os.listdir(model.work_path)
+    assert CICE_NML_NAME in work_path_files
+
+    shutil.rmtree(cloned_repo_path)
+
+
+@pytest.mark.parametrize("cice_config_files", 
+    [False, {"icefields_nml": { "f_icy": "m" }}],
+    indirect=True)
+def test_restart_clone(cice_config_files):
+
+    # To-do a restart clone we need to make a folder like archive/restart000 ?
+    with cd(ctrldir):
+            
+        lab = payu.laboratory.Laboratory(lab_path=str(labdir))
+        expt = payu.experiment.Experiment(lab, reproduce=False)
+        model = expt.models[0]
+
+        # Function to test
+        model.setup()
+
+    assert 'RESTART' in os.listdir(expt_workdir)
+
+    # Initialise a control repo
+    repo = git.Repo.init(ctrldir)
+    repo.index.add("*")
+    # Commit the changes
+    repo.index.commit("First commit - initialising repository")
+    source_main = str(repo.active_branch)
+
+    # Clone
+    cloned_repo_path = tmpdir / "clonedRepo"
+    clone(str(ctrldir), cloned_repo_path, lab_path=labdir, restart_path=ctrldir)
+
+    cloned_repo = git.Repo(cloned_repo_path)
+    cloned_repo.git.checkout(source_main)
+
+    ctrl_path_files = os.listdir(cloned_repo_path)
+    assert CICE_NML_NAME in ctrl_path_files
+    assert 'RESTART' in ctrl_path_files
+
+
+    if request.param:
+        assert HIST_NML_NAME in ctrl_path_files
+
+
+    # Setup
+
+    with cd(cloned_repo_path):
+
+        lab = payu.laboratory.Laboratory(lab_path=str(labdir))
+        expt = payu.experiment.Experiment(lab, reproduce=False)
+        model = expt.models[0]
+
+        # Function to test
+        model.setup()
+
+    work_path_files = os.listdir(model.work_path)
+    assert CICE_NML_NAME in work_path_files
+
+    shutil.rmtree(cloned_repo_path)
