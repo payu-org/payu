6d975159f4102ac184ab13544f1dbbb451e2225e
Jo Basevi <johanna.basevi@anu.edu.au>
2024-09-20 14:19:39 +1000

    Refactor up test_setup into multiple different tests for inputs and config files

--- test/test_setup.py
+++ test/test_setup.py
@@ -11,21 +11,12 @@ from .common import tmpdir, ctrldir, labdir, workdir
 from .common import payu_init, payu_setup
 from .common import config as config_orig
 from .common import write_config
-from .common import make_exe
+from .common import make_exe, make_inputs
 
 # Config files in the test model driver
 CONFIG_FILES = ['data', 'diag', 'input.nml']
+OPTIONAL_CONFIG_FILES = ['opt_data']
 INPUT_NML_FILENAME = 'input.nml'
-
-# INPUT PATHS
-INPUT_TO_PATHS = {
-    # Inputs in lab directory
-    "lab_inputs": labdir / 'input' / 'lab_inputs',
-    # Inputs in control directory
-    "ctrl_inputs": ctrldir / 'ctrl_inputs',
-    # Inputs in tmp directory - that will be symlinked to control directory
-    "tmp_inputs": tmpdir / 'tmp_inputs'
-}
 
 
 @pytest.fixture(autouse=True)
@@ -59,9 +50,40 @@ def test_init():
         assert((labdir / subdir).is_dir())
 
 
-def create_configuration_files():
-    """Create model config files in control directory"""
+def make_config_files():
+    """
+    Create files required for test model
+    """
     for file in CONFIG_FILES:
+        make_random_file(ctrldir/file, 29)
+
+
+def run_payu_setup(config=config_orig, create_inputs=False,
+                   create_config_files=True):
+    """Helper function to write config.yaml files, make inputs,
+    config files and run experiment setup"""
+    # Setup files
+    write_config(config)
+    make_exe()
+    if create_inputs:
+        make_inputs()
+    if create_config_files:
+        make_config_files()
+
+    # Initialise a payu laboratory
+    with cd(ctrldir):
+        payu_init(None, None, str(labdir))
+
+    # Run payu setup
+    payu_setup(lab_path=str(labdir))
+
+
+def test_setup_configuration_files():
+    """Test model config_files are copied to work directory,
+    and that any symlinks are followed"""
+    # Create configuration files
+    all_config_files = CONFIG_FILES + OPTIONAL_CONFIG_FILES
+    for file in all_config_files:
         if file != INPUT_NML_FILENAME:
             make_random_file(ctrldir / file, 8)
 
@@ -72,106 +94,79 @@ def create_configuration_files():
     input_nml_symlink.symlink_to(input_nml_realpath)
     assert input_nml_symlink.is_symlink()
 
+    # Run payu setup
+    run_payu_setup(create_inputs=True)
 
-def check_configuration_files():
-    """Test model config_files are copied to work directory,
-    and that any symlinks are followed"""
-    for file in CONFIG_FILES + ['config.yaml']:
+    # Check config files have been copied to work path
+    for file in all_config_files + ['config.yaml']:
         filepath = workdir / file
-        # Check file has been copied to work path
         assert filepath.exists() and filepath.is_file()
-
-        # Check file is not a symlink
         assert not filepath.is_symlink()
-
-        # Check file contents are copied
         assert filepath.read_bytes() == (ctrldir / file).read_bytes()
 
 
-def create_inputs():
-    """Create inputs in laboratory, control and a temporary
-    directory - that are symlinked to the control directory"""
+@pytest.mark.parametrize(
+    "input_path, is_symlink, is_absolute",
+    [
+        (labdir / 'input' / 'lab_inputs', False, False),
+        (ctrldir / 'ctrl_inputs', False, False),
+        (tmpdir / 'symlinked_inputs', True, False),
+        (tmpdir / 'tmp_inputs', False, True)
+    ]
+)
+def test_setup_inputs(input_path, is_symlink, is_absolute):
+    """Test inputs are symlinked to work directory,
+    and added in input manifest"""
     # Make inputs
-    for inputs, path in INPUT_TO_PATHS.items():
-        path.mkdir(parents=True, exist_ok=True)
-        for i in range(1, 4):
-            make_random_file(path / f'{inputs}_input_00{i}.bin',
-                            1000**2 + i)
+    input_path.mkdir(parents=True, exist_ok=True)
+    for i in range(1, 4):
+        make_random_file(input_path / f'input_00{i}.bin', 1000**2 + i)
 
-    # Create symlink in work directory
-    (ctrldir / 'tmp_inputs').symlink_to(INPUT_TO_PATHS['tmp_inputs'])
+    if is_symlink:
+        # Create an input symlink in control directory
+        (ctrldir / input_path.name).symlink_to(input_path)
 
+    # Modify config to specify input path
+    config = copy.deepcopy(config_orig)
+    config['input'] = str(input_path) if is_absolute else input_path.name
 
-def check_inputs():
-    """Check inputs are symlinked to work directory,
-    and added in input manifest
-    """
+    # Run payu setup
+    run_payu_setup(config=config, create_config_files=True)
+
     input_manifest = get_manifests(ctrldir/'manifests')['input.yaml']
-    for inputs, path in INPUT_TO_PATHS.items():
-        for i in range(1, 4):
-            filename = f'{inputs}_input_00{i}.bin'
-            work_input = workdir / filename
+    for i in range(1, 4):
+        filename = f'input_00{i}.bin'
+        work_input = workdir / filename
 
-            # Check file exists and file size is expected
-            assert work_input.exists() and work_input.is_symlink()
-            assert work_input.stat().st_size == 1000**2 + i
+        # Check file exists and file size is expected
+        assert work_input.exists() and work_input.is_symlink()
+        assert work_input.stat().st_size == 1000**2 + i
 
-            # Check relative input path is added to manifest
-            filepath = str(Path('work') / filename)
-            assert filepath in input_manifest
+        # Check relative input path is added to manifest
+        filepath = str(Path('work') / filename)
+        assert filepath in input_manifest
 
-            # Check manifest fullpath
-            expected_fullpath = path / filename
-            manifest_fullpath_str = input_manifest[filepath]['fullpath']
-            assert manifest_fullpath_str == str(expected_fullpath)
+        # Check manifest fullpath
+        manifest_fullpath = input_manifest[filepath]['fullpath']
+        assert manifest_fullpath == str(input_path / filename)
 
-            # Check fullpath is a resolved path
-            manifest_fullpath = Path(manifest_fullpath_str)
-            assert manifest_fullpath.is_absolute()
-            assert not manifest_fullpath.is_symlink()
-
-
-def check_exe(exe_name):
-    """Test executable has been added to work directory"""
-    bin_exe = labdir / 'bin' / exe_name
-    work_exe = workdir / exe_name
-    assert work_exe.exists() and work_exe.is_symlink()
-    assert work_exe.resolve() == bin_exe.resolve()
-
-
-def check_workdir(config):
-    """Test work directory is setup as expected"""
-    assert workdir.is_symlink and workdir.is_dir()
-
-    # Check configuration files, inputs and executables are moved to work dir
-    check_configuration_files()
-    check_inputs()
-    check_exe(config['exe'])
+        # Check fullpath is a resolved path
+        assert Path(manifest_fullpath).is_absolute()
+        assert not Path(manifest_fullpath).is_symlink()
 
 
 def test_setup():
-    """Test work directory is setup as expected, e.g. configuratiom files are
-    copied across, and inputs and executables are symlinked"""
-    config = copy.deepcopy(config_orig)
+    """Test work directory and executable are setup as expected,
+    and re-running setup requires a force=True"""
+    run_payu_setup(create_inputs=True, create_config_files=True)
 
-    # Create test configuration files, excutables, and inputs
-    create_configuration_files()
-    create_inputs()
-    make_exe()
+    assert workdir.is_symlink and workdir.is_dir()
 
-    # Over-ride input in default configuration
-    config['input'] = [input for input in INPUT_TO_PATHS]
-    write_config(config)
-
-    # Initialise a payu laboratory
-    with cd(ctrldir):
-        payu_init(None, None, str(labdir))
-
-    # Run setup
-    payu_setup(lab_path=str(labdir))
-
-    # Check files in work directory are setup as expected
-    check_workdir(config)
+    # Check executable symlink is in work directory
+    bin_exe = labdir / 'bin' / config_orig['exe']
+    work_exe = workdir / config_orig['exe']
+    assert work_exe.exists() and work_exe.is_symlink()
+    assert work_exe.resolve() == bin_exe.resolve()
 
     # Re-run setup - expect an error
     with pytest.raises(SystemExit,
@@ -179,11 +174,7 @@ def test_setup():
         payu_setup(lab_path=str(labdir), sweep=False, force=False)
     assert setup_error.type == SystemExit
 
-    # Re-run payu setup with force=True
-    payu_setup(lab_path=str(labdir), sweep=False, force=True)
-
-    # Check files in work directory are setup as expected
-    check_workdir(config)
+    assert workdir.is_symlink and workdir.is_dir()
 
 
 @pytest.mark.parametrize(
