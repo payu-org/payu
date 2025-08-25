c5ce7d3d55e1985ad7234a6edc548ae25ae84089
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2025-08-25 21:57:30 +1000

    Fix branch name check

--- payu/subcommands/sweep_cmd.py
+++ payu/subcommands/sweep_cmd.py
@@ -2,6 +2,7 @@
 
 from payu.experiment import Experiment
 from payu.laboratory import Laboratory
+from payu.git_utils import PayuBranchError
 import payu.subcommands.args as args
 
 title = 'sweep'
@@ -16,7 +17,15 @@ def runcmd(model_type, config_path, hard_sweep, lab_path, metadata_off):
     lab = Laboratory(model_type, config_path, lab_path)
     expt = Experiment(lab, metadata_off=metadata_off)
 
-    expt.sweep(hard_sweep)
+    try:
+        expt.sweep(hard_sweep)
+    except PayuBranchError as e:
+        # Check it is a detached HEAD state error before offering remedy
+        if "detached HEAD" in e:
+            sys.exit('payu: error:\n{e}.\n'
+                     'Checkout a branch before running payu sweep again.')
+        else:
+            raise
 
 
 runscript = runcmd
