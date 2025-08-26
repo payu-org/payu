91e6aabf36bc8efd52af86b533e2433ed2d91cd7
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2025-08-26 20:28:46 +1000

    Undo changes to sweep_cmd.py

--- payu/subcommands/sweep_cmd.py
+++ payu/subcommands/sweep_cmd.py
@@ -1,10 +1,7 @@
 # coding: utf-8
 
-import sys
-
 from payu.experiment import Experiment
 from payu.laboratory import Laboratory
-from payu.git_utils import PayuBranchError
 import payu.subcommands.args as args
 
 title = 'sweep'
@@ -17,16 +14,9 @@ arguments = [args.model, args.config, args.hard_sweep, args.laboratory,
 def runcmd(model_type, config_path, hard_sweep, lab_path, metadata_off):
 
     lab = Laboratory(model_type, config_path, lab_path)
-    try:
-        expt = Experiment(lab, metadata_off=metadata_off)
-    except PayuBranchError as e:
-        # Check it is a detached HEAD state error before offering remedy
-        if "detached HEAD" in str(e):
-            sys.exit(f'\npayu: error: {e}\n\n'
-                     'Checkout a branch before running payu sweep again.\n')
-        else:
-            raise
+    expt = Experiment(lab, metadata_off=metadata_off)
 
     expt.sweep(hard_sweep)
+
 
 runscript = runcmd
