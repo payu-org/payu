07c45dbea0bb5c34623d3157670aa6c0e3534231
Jasmeen Kaur <jasmeen.kaur@anu.edu.au>
2026-04-29 16:06:29 +1000

    Added docstring and replaced sys.exit with raise

--- payu/experiment.py
+++ payu/experiment.py
@@ -44,6 +44,7 @@ from payu.calendar import parse_date_offset
 from payu.sync import SyncToRemoteArchive
 from payu.metadata import Metadata
 import payu.telemetry as telemetry
+import payu.errors as errors
 
 # Environment module support on vayu
 # TODO: To be removed
@@ -475,8 +476,9 @@ class Experiment(object):
 
         # Confirm that no output path already exists
         if os.path.exists(self.output_path):
-            sys.exit('payu: error: Output path already exists: '
-                     '{path}.'.format(path=self.output_path))
+            raise errors.PayuError(f'Output path already exists: {self.output_path}')
+            # sys.exit('payu: error: Output path already exists: '
+            #          '{path}.'.format(path=self.output_path))
 
         # Confirm that no work path already exists
         if os.path.exists(self.work_path):
