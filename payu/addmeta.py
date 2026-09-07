c66909678b8918e38fd083e01a8ea13a8b4f7e59
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2026-09-07 17:04:51 +1000

    Remove notes

--- payu/addmeta.py
+++ payu/addmeta.py
@@ -4,60 +4,6 @@ addmeta tool
 :copyright: Copyright 2011 Marshall Ward, see AUTHORS for details.
 :license: Apache License, Version 2.0, see LICENSE for details.
 """
-"""
-do
-    addmeta \
-        -v -s \
-        -d metadata.yaml \
-        -d $PAYU_CURRENT_OUTPUT_DIR/env.yaml \
-        -m scripts/post-processing/addmeta/dataspec.yaml \
-        -m scripts/post-processing/addmeta/${submodel}.yaml \
-        --fnregex='access-esm1p6\.\w+(?:\.\dd)?\.(?P<var>\w+)\.(?P<freq>\w{2,4})(?:\.\w+)?(?:\.\d{4})?\.nc' \
-        $PAYU_CURRENT_OUTPUT_DIR/${submodel}/*.nc
-"""
-
-"""
-addmeta:
-    enable: true                               # default
-    verbose: true                              # default
-    update-history: false                      # default
-    data:
-        id: "{{ metadata.experiment_id }}"     # default
-        run_id: "{{ env.PAYU_RUN_ID }}"        # default
-    metafiles:
-        - metadata.yaml                        # default
-        - PATH_TO_OUTPUT_DIR/env.yaml          # default
-        - addmeta/dataspec.yaml
-    datafiles:
-        - more_metadata.yaml
-    fnregex: 'access-esm1p6\.\w+(?:\.\dd)?\.(?P<var>\w+)\.(?P<freq>\w{2,4})(?:\.\w+)?(?:\.\d{4})?\.nc'
-    submodel:
-        ocean:
-            metafiles:
-                - addmeta/ocean.yaml
-            fnregex: 'access-esm1p6\.\w+(?:\.\dd)?\.(?P<var>\w+)\.(?P<freq>\w{2,4})(?:\.\w+)?(?:\.\d{4})?\.nc'
-        ice:
-            metafiles:
-                - addmeta/ice.yaml
-        atmosphere:
-            metafiles:
-                - addmeta/atmosphere.yaml
-"""
-
-"""
-    # Call signature function to add metadata to model output directories
-    find_and_add_meta(
-        args.files,
-        combine_meta(metafiles),
-        kwdata,
-        args.fnregex,
-        sort_attrs=args.sort,
-        history=history,
-        verbose=verbose,
-    )
-"""
-
-
 from collections import defaultdict
 from types import SimpleNamespace
 
